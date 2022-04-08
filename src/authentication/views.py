4b3c3efad8005bc28eed9a76b999257d3fa3035f
Maria Gamal <maria.gamal13@gmail.com>
2022-04-08 17:01:59 +0200

    Update views.py

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -4,35 +4,6 @@ from django.views import View
 from django.contrib import auth, messages
 
 
-# class loginView(View):
-#     def get(self, request):
-#         return render(request=request, template_name='authentication/login.html')
-
-#     def post(self, request):
-#         username = request.POST['username']
-#         password = request.POST['password']
-
-#         if username and password:
-#             if user := auth.authenticate(username=username, password=password):
-#                 if user.is_active:
-#                     auth.login(request, user)
-#                     return redirect('index')
-
-#                 return render(request=request, template_name='auth/login.html')
-
-#             messages.error(request, 'Invalid username or password!')
-#             return render(request=request, template_name='auth/login.html')
-
-#         messages.error(request, 'Please fill all fields!')
-#         return render(request=request, template_name='auth/login.html')
-
-
-# class LogoutView(View):
-#     def post(self, request):
-#         auth.logout(request)
-#         messages.success(request, 'You have been logged out!')
-#         return redirect('login')
-
 def login(request):
     return render(
         request=request,
