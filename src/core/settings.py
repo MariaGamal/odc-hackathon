587b788e01ff5976ec6965adb0a414d92255d545
Maria Gamal <maria.gamal13@gmail.com>
2022-04-08 22:13:41 +0200

    Update settings.py

--- src/core/settings.py
+++ src/core/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'odc.egypt.learning@gmail.com'
-EMAIL_HOST_PASSWORD = '@dMin111'
+EMAIL_HOST_USER = '########################'
+EMAIL_HOST_PASSWORD = '###########'
